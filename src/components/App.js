ae2bfb92739a5b4d0b92205dbd93706beecd9218
Gremwiz <gremwiz@yandex.ru>
2021-06-14 01:37:37 +0300

    Rename Api.js -> api.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import ImagePopup from './ImagePopup';
-import api from '../utils/Api';
+import api from '../utils/api';
 import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
 import AddPlacePopup from './AddPlacePopup';
